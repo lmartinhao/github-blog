01859c309878837853e196f494f997a98711785d
lola martinhao <lola.martinhao@gmail.com>
2023-03-02 12:35:41 -0300

    header finished

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,29 @@
-import { EffectRectangle, HeaderContainer } from './styles'
+import {
+  EffectCircle1,
+  EffectCircle2,
+  EffectImage1,
+  EffectImage2,
+  EffectRectangle,
+  HeaderContainer,
+} from './styles'
 import Logo from '../../assets/Logo.svg'
+import Effect1 from '../../assets/effect1.svg'
+import Effect2 from '../../assets/effect2.svg'
 
 export function Header() {
   return (
     <HeaderContainer>
       <img src={Logo} alt="" />
+      <EffectImage1>
+        <img src={Effect1} alt="" />
+      </EffectImage1>
+      <EffectImage2>
+        <img src={Effect2} alt="" />
+      </EffectImage2>
+
       <EffectRectangle />
+      <EffectCircle1 />
+      <EffectCircle2 />
     </HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,10 +6,12 @@ export const HeaderContainer = styled.header`
   height: 18.5rem;
   text-align: center;
   padding-top: 4rem;
+
+  white-space: nowrap;
+  overflow: hidden;
 `
 
 export const EffectRectangle = styled.div`
-  position: absolute;
   width: 891px;
   height: 52px;
   left: calc(50% - 891px / 2 - 2.5px);
@@ -17,4 +19,40 @@ export const EffectRectangle = styled.div`
 
   background: #14589c;
   filter: blur(106px);
+`
+
+export const EffectImage1 = styled.div`
+  position: absolute;
+  width: 409px;
+  height: 188px;
+  left: 0px;
+  top: 70px;
+`
+export const EffectImage2 = styled.div`
+  position: absolute;
+  width: 371px;
+  height: 236px;
+  left: 1067px;
+  top: 30px;
+`
+export const EffectCircle1 = styled.div`
+  position: absolute;
+  width: 236px;
+  height: 236px;
+  left: -130px;
+  top: -96px;
+
+  background: #14589c;
+  filter: blur(200px);
+`
+export const EffectCircle2 = styled.div`
+  position: absolute;
+  width: 155.25px;
+  height: 155.25px;
+  right: -15.43px;
+  top: -52.14px;
+
+  background: #14589c;
+  filter: blur(184px);
+  transform: rotate(15deg);
 `
