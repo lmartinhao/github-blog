728d32397fc1da242eb2c4bd6017593d4bb9b76a
lola martinhao <lola.martinhao@gmail.com>
2023-03-02 11:23:02 -0300

    defying colors scheme

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,20 +1,16 @@
 export const defaultTheme = {
-  white: '#fff',
+  blue: '#3294F8',
 
-  'gray-100': '#E1E1E6',
-  'gray-300': '#C4C4CC',
-  'gray-400': '#8D8D99',
-  'gray-500': '#7C7C8A',
-  'gray-600': '#323238',
-  'gray-700': '#29292E',
-  'gray-800': '#202024',
-  'gray-900': '#121214',
+  'blue-100': '#E7EDF4',
+  'blue-200': '#C4D4E3',
+  'blue-300': '#AFC2D4',
+  'blue-400': '#7B96B2',
+  'blue-500': '#3A536B',
+  'blue-600': '#1C2F41',
+  'blue-700': '#112131',
+  'blue-800': '#0B1B2B',
+  'blue-900': '#071422',
 
-  'green-300': '#00B37E',
-  'green-500': '#00875F',
-  'green-700': '#015F43',
+  'gray-900': '#040F1A',
 
-  'red-300': '#F75A68',
-  'red-500': '#AB222E',
-  'red-700': '#7A1921',
 } as const
